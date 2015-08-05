f0af2cbb03afdba60c98d72d4ae849cf6c8a92cc
Cleverson <Cleverson@clever>
2015-08-05 19:46:41 -0300

    2

--- src/Qualquer.java
+++ src/Qualquer.java
@@ -1,6 +1,18 @@
+import com.sun.javafx.css.CalculatedValue;
+
 public class Qualquer {
 
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Olá Mundo");
+		
+		int res = calc(2, 2); 
+		
+		System.out.println(res);
+	}
+	
+	public static int calc(int a,int b){
+		return a+b;
 	}
 }
